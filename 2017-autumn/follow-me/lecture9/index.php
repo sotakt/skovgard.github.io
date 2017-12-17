b04be409f0e41a16c299cfa50ce33e2905ea9058
Jan Skovgård Jensen <jsj@eaaa.dk>
2017-12-17 19:40:36 +0100

    Add files via upload

---
+++ 2017-autumn/follow-me/lecture9/index.php
@@ -0,0 +1,138 @@
+<?php 
+// Include and instantiate the class.
+require_once 'Mobile_Detect.php';
+$detect = new Mobile_Detect;
+ 
+// Exclude tablets.
+if( $detect->isMobile() && !$detect->isTablet() ){
+  
+header("Location: http://janskovgard.dk/eaaa/17x1/lecture9/index-mobile.php");
+  exit();
+ 
+}
+?>
+<!DOCTYPE html>
+<html>
+<!-- Based on template from csslayoutgenerator.com -->
+
+<head>
+  <meta charset="utf-8" />
+  <!--[if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script><![endif]-->
+  <title>Boston Globe - Home</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="keywords" content="Boston Globe" />
+  <meta name="description" content="Boston Globe - Home" />
+  <link href="css/reset.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <div id="wrapper">
+    <nav id="navigation-icon"><a href="#navigation-mobile">Menu</a></nav>
+    <header class="header">
+      <h1><img src="images/boston_globe.svg" alt="Boston Globe logo"> </h1>
+
+
+    </header>
+    <!-- .header-->
+    <nav id="navigation-desktop">
+      <ul>
+        <li><a href="metro.html">Metro</a></li>
+        <li><a href="sports.html">Sports</a></li>
+        <li><a href="lifesyle.html">Lifestyle</a></li>
+        <li><a href="arts.html">Arts</a></li>
+      </ul>
+    </nav>
+
+    <main class="content">
+      <article id="article1">
+        <h2>If your town bans pot shops, should it get tax revenue?</h2>
+        <div class="author">By Dan Adams GLOBE STAFF OCTOBER 02, 2017</div>
+        <section>
+          <h3></h3>
+          <img src="images/pot2.jpg" alt="pot">
+          <p>Who should share in the spoils of the new recreational marijuana industry? If pro-pot advocates get their way, it won’t be any of the more than 100 municipalities that have enacted bans, moratoriums, or tight limits on licensed marijuana operations.
+          </p>
+          <p>Proponents of legalized marijuana are planning to push the Legislature to block cities and towns with such restrictions from receiving a single dime of the $150 million-plus in tax revenue the state could soon collect from retail cannabis sales.
+          </p>
+          <p>It’s a long-shot idea, but backers hope there’s appeal in its moral simplicity. Their argument amounts to the familiar parable of the Little Red Hen: Why should those who refused to help bake the bread get to eat it?
+          </p>
+          <p>“Municipalities shouldn’t be entitled to something they took no part in,” said Kamani Jefferson, who runs the Mass. Recreational Consumer Council. The proposal, he explained, “would force their hand and really encourage them to let these businesses in.”</p>
+        </section>
+
+      </article>
+
+      <article id="article2">
+        <h2>‘Whites Only USA’: Racist, pro-Trump graffiti found at Salem State</h2>
+        <section>
+          <div class="author">By J.D. Capelouto Globe Correspondent October 02, 2017</div>
+          <h3></h3>
+          <img src="images/salamstate.jpg" alt="Salem State">
+          <p>Officials in Salem are searching for the person responsible for vandalizing the Salem State University baseball field with blatantly racist and pro-Donald Trump graffiti on Friday.</p>
+
+          <p>
+            “Trump #1 Whites Only USA” and “Whites #1” were found spray-painted on benches and a fence surrounding the field Friday morning, according to photos from the scene. Another bench had graffiti that used the n-word and said “DIE.”</p>
+          <p>
+            Two days after the graffiti was found, it remained an “active criminal investigation,” Salem State president John Keenan said in a statement Sunday.</p>
+          <p>
+            If a student is found to be responsible, Keenan said, the punishment would be “immediate dismissal from the university and referral to the Commonwealth for potential crimi</p>
+        </section>
+      </article>
+      <article id="article3">
+        <h2>If your town bans pot shops, should it get tax revenue?</h2>
+        <div class="author">By Dan Adams GLOBE STAFF OCTOBER 02, 2017</div>
+        <section>
+          <h3></h3>
+          <img src="images/pot2.jpg" alt="pot">
+          <p>Who should share in the spoils of the new recreational marijuana industry? If pro-pot advocates get their way, it won’t be any of the more than 100 municipalities that have enacted bans, moratoriums, or tight limits on licensed marijuana operations.
+          </p>
+          <p>Proponents of legalized marijuana are planning to push the Legislature to block cities and towns with such restrictions from receiving a single dime of the $150 million-plus in tax revenue the state could soon collect from retail cannabis sales.
+          </p>
+          <p>It’s a long-shot idea, but backers hope there’s appeal in its moral simplicity. Their argument amounts to the familiar parable of the Little Red Hen: Why should those who refused to help bake the bread get to eat it?
+          </p>
+          <p>“Municipalities shouldn’t be entitled to something they took no part in,” said Kamani Jefferson, who runs the Mass. Recreational Consumer Council. The proposal, he explained, “would force their hand and really encourage them to let these businesses in.”</p>
+        </section>
+
+      </article>
+
+      <article id="article4">
+        <h2>‘Whites Only USA’: Racist, pro-Trump graffiti found at Salem State</h2>
+        <section>
+          <div class="author">By J.D. Capelouto Globe Correspondent October 02, 2017</div>
+          <h3></h3>
+          <img src="images/salamstate.jpg" alt="Salem State">
+          <p>Officials in Salem are searching for the person responsible for vandalizing the Salem State University baseball field with blatantly racist and pro-Donald Trump graffiti on Friday.</p>
+
+          <p>
+            “Trump #1 Whites Only USA” and “Whites #1” were found spray-painted on benches and a fence surrounding the field Friday morning, according to photos from the scene. Another bench had graffiti that used the n-word and said “DIE.”</p>
+          <p>
+            Two days after the graffiti was found, it remained an “active criminal investigation,” Salem State president John Keenan said in a statement Sunday.</p>
+          <p>
+            If a student is found to be responsible, Keenan said, the punishment would be “immediate dismissal from the university and referral to the Commonwealth for potential crimi</p>
+        </section>
+      </article>
+    </main>
+    <!-- .content -->
+    <footer class="footer">
+      <h2>Footer:</h2>
+      <p>Copyrights, address etc. - Stuff you repeat on all your pages ...</p>
+      <address>jsj@baaa.dk</address>
+      <!-- Based on template from csslayoutgenerator.com -->
+
+    </footer>
+    <!-- .footer -->
+    <nav id="navigation-mobile">
+      <ul>
+        <li><a href="metro.html">Metro</a></li>
+        <li><a href="sports.html">Sports</a></li>
+        <li><a href="lifesyle.html">Lifestyle</a></li>
+        <li><a href="arts.html">Arts</a></li>
+      </ul>
+    </nav>
+  </div>
+  <!-- .wrapper -->
+
+</body>
+
+</html>
